8118336676930f04a3954b37c21fbabac537262e
Ashish Jaiswal <jaiswalashish025@gmail.com>
2023-12-13 14:48:24 +0530

    created a product page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -44,3 +44,10 @@ func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request)
 		return
 	}
 }
+
+// ChargeOnce displays the page to buy one widget
+func (app *application) ChargeOnce(w http.ResponseWriter, r *http.Request) {
+	if err := app.renderTemplate(w, r, "buy-once", nil); err != nil {
+		app.errorlog.Println(err)
+	}
+}
