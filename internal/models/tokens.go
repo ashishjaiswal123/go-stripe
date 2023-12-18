487324af42c9639b600837780d5803dc89001822
Ashish Jaiswal <jaiswalashish025@gmail.com>
2023-12-18 21:03:09 +0530

    check for expiry of user

--- internal/models/tokens.go
+++ internal/models/tokens.go
@@ -53,14 +53,15 @@ func (m *DBModel) InsertToken(t *Token, u User) error {
 		return err
 	}
 
-	stmt = `insert into tokens (user_id, name, email, token_hash, created_at, updated_at)
-		values (?, ?, ?, ?, ?, ?)`
+	stmt = `insert into tokens (user_id, name, email, token_hash, expiry,  created_at, updated_at)
+		values (?, ?, ?, ?, ?, ?, ?)`
 
 	_, err = m.DB.ExecContext(ctx, stmt,
 		u.ID,
 		u.LastName,
 		u.Email,
 		t.Hash,
+		t.Expiry,
 		time.Now(),
 		time.Now(),
 	)
