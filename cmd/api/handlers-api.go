1d18344cf65fd2e083499bd0db4408783208ce2a
Ashish Jaiswal <jaiswalashish025@gmail.com>
2023-12-20 12:01:48 +0530

    implemented routes, handler, all-subscriptions template to show subscriptions list

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -525,3 +525,13 @@ func (app *application) AllSales(w http.ResponseWriter, r *http.Request) {
 
 	app.writeJSON(w, http.StatusOK, allSales)
 }
+
+func (app *application) AllSubscriptions(w http.ResponseWriter, r *http.Request) {
+	allSubscriptions, err := app.DB.GetAllSubscriptions()
+	if err != nil {
+		app.badRequest(w, r, err)
+		return
+	}
+
+	app.writeJSON(w, http.StatusOK, allSubscriptions)
+}
